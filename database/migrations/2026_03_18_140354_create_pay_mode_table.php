89a45a52f8958e7f6d5e0888b9b94115f5e18774
ingtidesarrollador05-netizen <ingti.desarrollador05@unicatolica.edu.con>
2026-03-18 09:05:36 -0500

    gia

--- database/migrations/2026_03_18_140354_create_pay_mode_table.php
+++ database/migrations/2026_03_18_140354_create_pay_mode_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('pay_mode', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 80);
-            $table->string('description');
+            $table->string('name', 50);
+            $table->string('observation', 200);
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('pay_mode');
     }
 };
